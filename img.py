d4f248ae0b871a4ed9cc1cc79cafd8dd5910485b
mtueda <marcosueda95@gmail.com>
2020-01-09 00:13:29 +0100

    update

--- img.py
+++ img.py
@@ -2,12 +2,13 @@ import cv2
 import numpy as np
 import os
 import glob
-from paint import paint
+from paintAfterCrop import paint
 
 
-img = glob.glob('*pos.png')
+img = glob.glob('DSCN1650.JPG')
+#img = glob.glob('*.JPG')
 
 for img_path in img:
-	paint(img_path,46,1703,391,167,591,140,917,1563)
+	paint(img_path,46,1703,391,167,591,140,917,1563)#(x(number of collumns),y(number of lines))bottom left corner, top left corner, top right corner, bottom right corner
 	
 
